940a4c5f545596e4b79a14d321931fe66f08d4d8
yueunkyung <48663193+yueunkyung@users.noreply.github.com>
2023-11-06 20:43:23 +0900

    [level unrated] Title: 배열의 유사도, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/unrated/120903. 배열의 유사도/배열의 유사도.js
@@ -1,9 +1,7 @@
 function solution(s1, s2) {
     var answer = 0;
     for(var i=0; i<s1.length; i++) {
-        for(var j=0; j<s2.length; j++) {
-            if(s1[i]===s2[j]) answer++;
-        }
+        if(s2.includes(s1[i])) answer++;
     }
     
     return answer;
